512d5de717362ca9a7e96c04edc46b7d615e06e5
Satchit R <116943067+satchit-r@users.noreply.github.com>
2024-07-08 15:47:27 +0530

    Delete HRMS_BACKEND directory

--- HRMS_BACKEND/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-//src/routes/authRoutes.js
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-router.post("/login", authController.login);
-
-module.exports = router;
